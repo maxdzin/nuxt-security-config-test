5793e771513ec392b518ce55a17ea2042d20fc9a
Max Dzin <6406412+maxdzin@users.noreply.github.com>
2024-10-24 11:17:56 +0300

    chore: security settings update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,46 @@ export default defineNuxtConfig({
   },
 
   routeRules: {
+    '/': {
+      security: {
+        // headers: {
+        //   contentSecurityPolicy: {
+        //     'frame-ancestors': [
+        //       "'self'",
+        //     ],
+        //   },
+        // },
+        corsHandler: {
+          origin: [
+            process.env.NUXT_PUBLIC_APP_BASE_URL as string,
+          ],
+        },
+        rateLimiter: {
+          tokensPerInterval: 8,
+          interval: 10000,
+        },
+      },
+    },
+    '/test': {
+      security: {
+        // headers: {
+        //   contentSecurityPolicy: {
+        //     'frame-ancestors': [
+        //       "'self'",
+        //     ],
+        //   },
+        // },
+        corsHandler: {
+          origin: [
+            process.env.NUXT_PUBLIC_APP_BASE_URL as string,
+          ],
+        },
+        rateLimiter: {
+          tokensPerInterval: 8,
+          interval: 10000,
+        },
+      },
+    },
     '/custom': {
       security: {
         // headers: {
@@ -24,6 +64,39 @@ export default defineNuxtConfig({
         //     ],
         //   },
         // },
+        corsHandler: {
+          origin: [
+            process.env.NUXT_PUBLIC_APP_BASE_URL as string,
+            'https://example.com',
+            'https://www.example.com',
+          ],
+        },
+        rateLimiter: {
+          tokensPerInterval: 8,
+          interval: 10000,
+        },
+      },
+    },
+    '/api/statistics/custom': {
+      security: {
+        corsHandler: {
+          origin: ['https://example.com', 'https://www.example.com'],
+        },
+        rateLimiter: {
+          tokensPerInterval: 8,
+          interval: 10000,
+        },
+      },
+    },
+    '/api/statistics/entries': {
+      security: {
+        corsHandler: {
+          origin: [process.env.NUXT_PUBLIC_APP_BASE_URL as string],
+        },
+        rateLimiter: {
+          tokensPerInterval: 8,
+          interval: 10000,
+        },
       },
     },
   },
